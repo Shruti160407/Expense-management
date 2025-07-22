ec96122a963e6377920cccda3f7c6b6086f01c0f
Shruti160407 <shrutichourasia1607@gmail.com>
2025-07-23 01:56:05 +0530

    Defining App Rotes

--- frontend/expense-management/src/App.jsx
+++ frontend/expense-management/src/App.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
+
 const App = () => {
   return (
-    <div className='text-3xl text-amber-900'>App</div>
+    <div className='text-3xl text-amber-900'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/SignUp" exact element={<SignUp />} />
+          <Route path="/Home" exact element={<Home />} />
+          <Route path="/Income" exact element={<Income />} />
+          <Route path="/Expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
-export default App
+export default App;
+
+const Root = () => {
+  //check if token exists in localStorage 
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
