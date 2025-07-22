48d20cef8578a73dc662fed6625a1f95952bbe4d
Shruti160407 <shrutichourasia1607@gmail.com>
2025-07-23 02:40:22 +0530

    Login Page setup

--- frontend/expense-management/src/components/layouts/AuthLayout.jsx
+++ frontend/expense-management/src/components/layouts/AuthLayout.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import CARD_2 from '../../assets/images/card2.png';
 
-const AuthLayout = () => {
+const AuthLayout = ({ children }) => {
   return (
-    <div>AuthLayout</div>
+    <div className='flex'>
+    <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
+        <h2 className='text-lg font-medium text-violet-950'>Expense Management</h2>
+        {children}
+    </div> 
+
+<div className='hidden md:block w-[4ovw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'></div>
+<div className='w-48 h-48 rounded-[48px] bg-purple-600 absolute -top-7 -left-5'></div>
+<div className='w-48 h-56 rounded-[48px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-10'></div>
+<div className='w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5'></div>
+
+<div className='grid grid-cols-1 z-20'>
+    <statsInfoCard
+    icon={<LuTrendingUpDown />}
+    label="Track your Income and Expenses"
+    value="430,000"
+    colors="bg-primary"
+    />
+</div>
+
+<img 
+src={CARD_2}
+className='w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-400/15'
+/>
+    </div>
   )
 }
 
